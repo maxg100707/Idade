4dc97313bc550cc6c69196e2cc178c6586460e0f
maxg100707 <maxguzman914@gmail.com>
2025-01-03 11:54:03 -0300

    Update idade.js

--- idade.js
+++ idade.js
@@ -53,3 +53,24 @@ function verificar() {
     res.appendChild(img);
   }
 }
+/*
+        _             _
+       / \           / \
+      /   \         /   \
+     /     \_______/     \
+    /  ___           ___  \
+   |  / ()\         / ()\  |
+   |  \___/         \___/  |
+   |           ___         |
+   \          |___|        /
+    \           |         /
+     \      \___|___/    /
+      \_               _/
+        \_____________/  
+           MIAW MIAW
+
+
+instagram : https://www.instagram.com/maxg1007/
+portifolio : https://maxg100707.github.io/Portifolio/
+
+*/
